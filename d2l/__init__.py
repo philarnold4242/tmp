1aaeb0d11afc436f58ee70367fd7cf9e370347ca
Phil Arnold <phil.arnold@gmail.com>
2025-10-27 12:19:51 +0100

    Test.

---
+++ d2l/__init__.py
@@ -0,0 +1,1 @@
+from .d2l import *
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='d2l',
+    version='0.1.0',
+    packages=find_packages(), # This automatically finds the 'd2l' directory
+    # other metadata like author, description, install_requires, etc.
+)
